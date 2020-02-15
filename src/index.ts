25d7b4c1dc22716739f19d1e9822b966522d354e
Mihai-Tudor Popescu <mihai.tudor.popescu@gmail.com>
2020-02-15 16:05:16 +0200

    Added enum

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,27 @@ const matches = fs.readFileSync('./resources/football.csv', {encoding: 'utf-8'})
     return record.split(',');
 });
 
+enum MatchResult {
+    HomeWin= 'H',
+    AwayWin= 'A',
+    Draw= 'D'
+}
+
 let manUtdWins = 0;
+
+function isAwayWin(record: string[]) {
+    return MatchResult.AwayWin === record[5];
+}
+
+function isHomeWin(record: string[]) {
+    return MatchResult.HomeWin === record[5];
+}
+
 for (let record of matches){
     const MANCHESTER_UTD = 'Man United';
-    if (MANCHESTER_UTD === record[1] && 'H' === record[5] || MANCHESTER_UTD === record[2] && 'A' === record[5]) {
+
+    if (MANCHESTER_UTD === record[1] && isHomeWin(record) || MANCHESTER_UTD === record[2] && isAwayWin(record)) {
         manUtdWins++;
-        console.log(record);
     }
 }
 
