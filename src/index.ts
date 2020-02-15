9b8c0de42404d809b66010dc63ac188946c90473
Mihai-Tudor Popescu <mihai.tudor.popescu@gmail.com>
2020-02-15 15:47:07 +0200

    Little statistics

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,14 @@ import fs from 'fs';
 const matches = fs.readFileSync('./resources/football.csv', {encoding: 'utf-8'}).split('\n').map((record)=>{
     return record.split(',');
 });
-console.log(matches);
+
+let manUtdWins = 0;
+for (let record of matches){
+    const MANCHESTER_UTD = 'Man United';
+    if (MANCHESTER_UTD === record[1] && 'H' === record[5] || MANCHESTER_UTD === record[2] && 'A' === record[5]) {
+        manUtdWins++;
+        console.log(record);
+    }
+}
+
+console.log(`Manchester United won ${manUtdWins} matches`);
