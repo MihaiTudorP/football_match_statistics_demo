cf4689b6f9fc2c31d6c387f6619b4530ddf9211e
Mihai-Tudor Popescu <mihai.tudor.popescu@gmail.com>
2020-02-16 01:32:30 +0200

    Refactored with composition

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,20 @@
 import {CsvFileReader} from './classes/CsvFileReader';
 import {MatchResult} from './enums/MatchResult';
+import {MatchReader} from './classes/MatchReader';
 
+
+// create an object that satisfies the DataReader interface
+// create a MatchReader and pass the object satisfying DataReader
 const reader = new CsvFileReader('./resources/football.csv');
-reader.read();
+const matchReader = new MatchReader(reader);
+matchReader.load();
 
 let manUtdWins = 0;
 
-function isAwayWin(record: string[]) {
-    return MatchResult.AwayWin === record[5];
-}
-
-function isHomeWin(record: string[]) {
-    return MatchResult.HomeWin === record[5];
-}
-
-for (let record of reader.data){
+for (let record of matchReader.matches){
     const MANCHESTER_UTD = 'Man United';
 
-    if (MANCHESTER_UTD === record[1] && isHomeWin(record) || MANCHESTER_UTD === record[2] && isAwayWin(record)) {
+    if (MANCHESTER_UTD === record[1] && MatchResult.HomeWin === record[5] || MANCHESTER_UTD === record[2] && MatchResult.AwayWin === record[5]) {
         manUtdWins++;
     }
 }
