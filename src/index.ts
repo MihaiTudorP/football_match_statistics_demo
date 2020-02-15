73a4475b38189b83b68a50942789f563a5a9d01a
Mihai-Tudor Popescu <mihai.tudor.popescu@gmail.com>
2020-02-16 01:33:39 +0200

    Fixed statistics logic

--- src/index.ts
+++ src/index.ts
@@ -6,18 +6,10 @@ reader.read();
 
 let manUtdWins = 0;
 
-function isAwayWin(record: string[]) {
-    return MatchResult.AwayWin === record[5];
-}
-
-function isHomeWin(record: string[]) {
-    return MatchResult.HomeWin === record[5];
-}
-
 for (let record of reader.data){
     const MANCHESTER_UTD = 'Man United';
 
-    if (MANCHESTER_UTD === record[1] && isHomeWin(record) || MANCHESTER_UTD === record[2] && isAwayWin(record)) {
+    if (MANCHESTER_UTD === record[1] && MatchResult.HomeWin === record[5] || MANCHESTER_UTD === record[2] && MatchResult.AwayWin === record[5]) {
         manUtdWins++;
     }
 }
